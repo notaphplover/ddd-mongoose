5223939c5b6750215980fad7d943ab41484d90c3
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-12-25 17:25:14 +0100

    feat(db): add db env fixtures

---
+++ src/layer-modules/db/test/fixtures/infrastructure/env/fixtures.ts
@@ -0,0 +1,18 @@
+import {
+  DeepCloneFixtureFactory,
+  FixtureFactory,
+} from '../../../../../../common/test';
+import { DbDotEnvVariables } from '../../../../infrastructure/env/DbDotEnvVariables';
+
+const dbDotEnvVariables: DbDotEnvVariables = {
+  MONGO_CONNECTION_AUTH_SOURCE: 'source',
+  MONGO_CONNECTION_DB: 'db',
+  MONGO_CONNECTION_PASSWORD: 'password',
+  MONGO_CONNECTION_PROTOCOL: 'protocol',
+  MONGO_CONNECTION_URL: 'url',
+  MONGO_CONNECTION_USER: 'user',
+};
+
+export const dbDotEnvVariablesFixtureFactory: FixtureFactory<DbDotEnvVariables> = new DeepCloneFixtureFactory(
+  dbDotEnvVariables,
+);
